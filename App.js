5e7ddc1260b7e3e1ecf5f8ac8760b9ea1a333d84
dstrigoun <dstrigoun@outlook.com>
2020-02-28 13:27:41 -0800

    basic splash screen

--- App.js
+++ App.js
@@ -1,19 +1,32 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  StatusBar
+} from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>test</Text>
-    </View>
-  );
+export default class App extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.logo}>
+          *LOGO*
+        </Text>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#4F6D7A',
+  },
+  logo: {
+    fontSize: 80,
+    textAlign: 'center',
+    margin: 40,
+    color: '#F5FCFF',
   },
 });
