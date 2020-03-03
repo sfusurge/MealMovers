5842e55b2eadd24cf1a6d1d9ca39b67501dc3d25
Praneer Shrestha <praneer.shrest@gmail.com>
2020-03-02 16:06:54 -0800

    requests and cancellations w/o button flow

--- App.js
+++ App.js
@@ -1,5 +1,7 @@
 import * as React from 'react';
 import Nav from './components/Nav.js';
+import Request from './components/Request.js';
+import FoodRequested from './components/FoodRequested.js'
 
 export default function App() {
   return (
