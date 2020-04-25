a63dd7a92bfec5d299d45ee93c07a11f26aa11ce
Joshua Peng <46659122+JCODEPENG@users.noreply.github.com>
2020-04-24 19:19:29 -0700

    Added target points
    Still trying to get the the get current position to work

--- components/Map.js
+++ components/Map.js
@@ -1,12 +1,70 @@
-import MapView from 'react-native-maps'
-import { StyleSheet, View, Image, Text, KeyboardAvoidingView, TouchableOpacity, TextInput, Dimensions  } from 'react-native'
+import { StyleSheet, View, Image, Text, KeyboardAvoidingView, TouchableOpacity, TextInput, Dimensions, Platform  } from 'react-native'
 import React, { Component } from 'react';
 import { useNavigation } from '@react-navigation/native';
+import MapView,{PROVIDER_GOOGLE, Marker, Callout} from 'react-native-maps'
+
+//import {request,PERMISSIONS} from 'react-native-permissions'
 
 export default function Map({navigation}) {
+    let thisLatitude
+    let thisLongitude
+    var _map
+
+    
+
+    // requestLocationPermission = async () => {
+    //     if (Platform.OS === 'ios'){
+    //         var response = await request(PERMISSIONS.IOS.LOCATION_WHEN_IN_USE)
+    //         console.log('Iphone' + response)
+    //     }
+    //     if (response === 'granted'){
+    //         locateCurrentPos();
+    //     } 
+    // }
+
+    const locateCurrentPos = (thisLatitude,thisLongitude) => {
+        navigator.geolocation.getCurrentPosition(
+            position => {
+                console.log("Check this statement")
+                console.log(JSON.stringify(position))
+                thisLatitude = position.coords.latitude,
+                thisLongitude = position.coords.longitude                
+            }   
+        )
+    }
+
+
+
+
     return(
         <View style={styles.container}>
-            <MapView style={styles.mapStyle} />
+            <Text>{thisLatitude}</Text>            
+            <MapView 
+            provider = {PROVIDER_GOOGLE} 
+            //ref = {map=> _map = map}
+            showsUserLocation = {true}
+            style = {styles.mapStyle}
+            region = {{
+                latitude: thisLatitude,
+                longitude: thisLongitude,
+                latitudeDelta: 0.09,
+                latitudeDelta: 0.035
+            }}>
+                <Marker
+                coordinate  = {{latitude: 49.278094, longitude: -122.919883}}
+                title = {'Meal Movers Home Base'}>
+
+                <Callout>
+                    <Image 
+                    style = {styles.logo}
+                    source = {require('./Images/UIHere.png')}
+                    />
+                    <Text>Meal Movers Home Base</Text>
+                </Callout>
+
+                </Marker>
+            </MapView>
+
         </View>
     )
 }
@@ -18,9 +76,13 @@ const styles = StyleSheet.create({
       alignItems: 'center',
       justifyContent: 'center',
     },
+    logo: {
+        width : 100,
+        height : 100
+    },
     mapStyle: {
-        width: Dimensions.get('window').width,
-        height: Dimensions.get('window').height,
+        width: '100%',
+        height: '80 %'
       },
 
   });
